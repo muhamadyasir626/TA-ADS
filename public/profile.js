e46d186f063b7438d748e3ec8bae9057088be37e
adityamhdr10 <88261982+adityamahendrapro@users.noreply.github.com>
2024-05-17 17:36:23 +0700

    update prifle

---
+++ public/profile.js
@@ -0,0 +1,20 @@
+const imgDiv = document.querySelector(".user");
+const img = document.querySelector('#photo');
+const file = document.querySelector('#file');
+const uploadebtn = document.querySelector('#uploadbtn');
+
+
+
+file.addEventListener('change', function(){
+const chosedfile = this.Files[0];
+if(chosedfile){
+    const reader = new FileReader();
+
+    reader.addEventListener( 'load' , function(){
+        img.setAttribute('src', reader.result);
+    })
+ reader.readAsDataURL (chosedfile);
+
+}
+
+})
